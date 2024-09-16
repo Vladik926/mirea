d46f8efcdeae0968879df4f5e2c80abb6f614662
Vladik926 <tuchinvlad2005@mail.ru>
2024-09-16 19:13:56 +0300

    Update my1.py

--- my1.py
+++ my1.py
@@ -1,3 +1,4 @@
+№1.3
 x = 5 >= 2
 A = {1, 3, 7, 8}
 B = {2, 4, 5, 10, 'apple'}
@@ -14,3 +15,11 @@ print("C:", C, "Тип:", type(C))
 print("df:", df, "Тип:", type(df))
 print("z:", z, "Тип:", type(z))
 print("D:", D, "Тип:", type(D))
+Ответ:
+x: True Тип: <class 'bool'>
+A: {8, 1, 3, 7} Тип: <class 'set'>
+B: {2, 4, 5, 10, 'apple'} Тип: <class 'set'>
+C: set() Тип: <class 'set'>
+df: ['Антонова Антонина', 34, 'ж'] Тип: <class 'list'>
+z: type Тип: <class 'str'>
+D: [1, 'title', 2, 'content'] Тип: <class 'list'>

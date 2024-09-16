16ea9e10f5f9c23a22827c0e9a43be7464774d1f
Vladik926 <tuchinvlad2005@mail.ru>
2024-09-16 19:10:42 +0300

    Add files via upload

---
+++ my1.py
@@ -0,0 +1,16 @@
+x = 5 >= 2
+A = {1, 3, 7, 8}
+B = {2, 4, 5, 10, 'apple'}
+C = A & B
+
+df = ['Антонова Антонина', 34, 'ж']
+z = 'type'  # Строка z
+D = [1, 'title', 2, 'content']
+
+print("x:", x, "Тип:", type(x))
+print("A:", A, "Тип:", type(A))
+print("B:", B, "Тип:", type(B))
+print("C:", C, "Тип:", type(C))
+print("df:", df, "Тип:", type(df))
+print("z:", z, "Тип:", type(z))
+print("D:", D, "Тип:", type(D))
